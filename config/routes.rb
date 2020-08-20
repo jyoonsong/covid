da689e458c6d85455db8fed5fdcf7e6c292da089
Jaeyoon Song <jyo3on@gmail.com>
2020-08-20 14:27:20 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "languages#index"
+  get "/:id", to: "languages#index"
+  get "/:id/:si_id", to: "languages#index"
+  post "/:id/:si_id", to: "languages#index"
+  
 end
